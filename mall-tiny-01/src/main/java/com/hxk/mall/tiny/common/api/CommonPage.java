86d7abeafc5ea73baa700f62c1d1faa09659e07b
hxk <1429195907@qq.com>
2021-08-29 12:55:02 +0800

    sleep

---
+++ mall-tiny-01/src/main/java/com/hxk/mall/tiny/common/api/CommonPage.java
@@ -0,0 +1,11 @@
+package com.hxk.mall.tiny.common.api;
+
+/**
+ * @ClassName CommonPage
+ * @Description TODO
+ * @Author OvO
+ * @Date 2021-08-29 11:27
+ * @Version 1.0
+ **/
+public class CommonPage {
+}
